59732cc47d3770198b3503e761532df6e2c951f4
Ali <alixdehghani@gmail.com>
2018-12-31 18:09:26 +0330

    add snackbar for login and singin error massage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,15 @@
 import { AuthData } from './auth-data.model';
-import { User } from './user.model';
 import { Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth'
 import { TrainingService } from '../training/training.service';
+import { MatSnackBar } from '@angular/material';
 @Injectable()
 export class AuthService {
     authChange = new Subject<boolean>();
     isAuthentication = false;
-    constructor(private router: Router, private afAuth: AngularFireAuth, private trainingService: TrainingService) { }
+    constructor(private router: Router, private afAuth: AngularFireAuth, private trainingService: TrainingService, private snackBar: MatSnackBar) { }
     initAuthLisiner() {
         this.afAuth.authState.subscribe(user => {
             if (user) {
@@ -25,10 +25,24 @@ export class AuthService {
         });
     }
     registerUser(authData: AuthData) {
-        this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password);
+        this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password).then(massage =>{
+            // console.log(massage);
+            
+        }).catch(error => {
+            // console.log(error);
+            this.snackBar.open(error.message, null, {
+                duration: 2000,
+              });
+        });
     }
     loginUser(authData: AuthData) {
-        this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password);
+        this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password).then(massage => {
+            console.log(massage);  
+        }).catch(error => {
+            this.snackBar.open(error.message, null, {
+                duration: 2000,
+              });
+        });
     }
     logoutUser() {
         this.afAuth.auth.signOut();
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule } from '@angular/material'
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule, MatSnackBarModule } from '@angular/material'
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule, MatSnackBarModule],
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule, MatSnackBarModule]
 })
 export class MaterialModule {
 
